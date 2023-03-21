9b3dcc4ff55a323b266a1debca2db0145e403a3f
totarochristian <totaro.christian.18@gmail.com>
2023-03-21 15:16:51 +0100

    Add the function to get random int in the utility file

---
+++ js/utility.js
@@ -0,0 +1,11 @@
+/**
+ * Function that will generate a random int
+ * @param {bigint} max Max value of the interval
+ * @param {bigint} min Min value of the interval
+ * @returns {bigint} Generated random int value
+ */
+function GetRandomInt(max,min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
